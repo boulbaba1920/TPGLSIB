233e4162708c3503ecf63f1c114fcd973f5a6e5d
boulbaba_ellabad <boulbaba.ellabad@tek-up.de>
2021-12-12 20:05:44 +0100

    TP blockchain

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -10,25 +10,47 @@ contract Election{
      struct Candidate {
          uint id;
          string name;
+         string avatar
          uint voteCount;
          }
 
          mapping(uint => Candidate) public candidates;
+         mapping(uint => bool) public voters;
 
          uint public candidatesCount ;
+         uint public totalvotes;
 
+event votedEvent
+(
+    uint indexed _candidateId
+);
 
     // Constructor 
     constructor() public {
-        addcandidate("boulbaba");
-        addcandidate("mohamed");
-        addcandidate("amin");
-        addcandidate("ilyes");
+        addcandidate("Chayma","yrl1");
+        addcandidate("Wassim","yrl2");
+        addcandidate("Oumayma","yrl3");
+        addcandidate("Amine","yrl4");
+        
      }
 
 
-function addCandidate (stringmemory_name) private {
+function addCandidate (string memory_name,string memory _url) private {
     candidatesCount ++;
     candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
    
+   cast Vote:function(){ 
+       var candidateId=$('#candidatesSelect').val();
+       App.contracts.Election.deployed().then(function(instance)
+       {
+           returninstance.vote(candidateId,{from:App.account});
+           }).then(function(result){
+               //Waitforvotestoupdate
+               $("#content").hide();
+               $("#loader").show();
+               }).catch(function(err){
+                   console.error(err);
+                   });
+                   }
+                   };
